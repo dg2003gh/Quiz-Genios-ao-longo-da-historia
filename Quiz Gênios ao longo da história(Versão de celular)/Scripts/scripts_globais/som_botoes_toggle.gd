91d5e0bce28ba9eb8ac0145e750684d279ddfaac
Douglas Gabriel de Oliveira Guimarães <dg2003@pop-os.localdomain>
2023-06-29 07:11:43 -0300

    add "gênios ao longo da história" to this branch

--- Quiz Gênios ao longo da história(Versão de celular)/Scripts/scripts_globais/som_botoes_toggle.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-func _ready(): 
-	self.connect("pressed", Callable(self, "_tocar_som_botoes")) #não consegui achar um lugar que me diga como usar a namestring de uma função de um autoload. :( se achar, é só mandar pull.
-	
-func _tocar_som_botoes():
-	Audio._tocar_som_botoes(true)
-	
